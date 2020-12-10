696d5294bedbdaa55a00ee8aed22d9263ce5e38a
Max-Gabriel-Susman <gabe.susman@gmail.com>
2020-12-09 23:29:16 -0700

    first commit

---
+++ EntityFrameworkExploration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntityFrameworkExploration
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
